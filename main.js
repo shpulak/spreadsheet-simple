c3de0299d7c60fa4c23198fd0d56d69f20c5c504
Shashwat <shashwat.pulak@gmail.com>
2018-07-19 04:26:05 +0530

    Add all event handlers

--- main.js
+++ main.js
@@ -34,7 +34,22 @@ createHeaderRow = () => {
   for (let i = 0; i <= defaultColCount; i++) {
     const th = document.createElement("th");
     th.setAttribute("id", `0-${i}`);
-    th.innerHTML = i === 0 ? `` : `Col ${i}`;
+    th.setAttribute("class", `${i === 0 ? "" : "column-header"}`);
+    // th.innerHTML = i === 0 ? `` : `Col ${i}`;
+    if (i !== 0) {
+      const span = document.createElement("span");
+      span.innerHTML = `Col ${i}`;
+      const dropDownDiv = document.createElement("div");
+      dropDownDiv.setAttribute("class", "dropdown");
+      dropDownDiv.innerHTML = `<button class="dropbtn" id="col-dropbtn-${i}">V</button>
+        <div id="col-dropdown-${i}" class="dropdown-content">
+          <p class="col-insert-left">Insert 1 column left</p>
+          <p class="col-insert-right">Insert 1 column right</p>
+          <p class="col-delete">Delete column</p>
+        </div>`;
+      th.appendChild(span);
+      th.appendChild(dropDownDiv);
+    }
     tr.appendChild(th);
   }
   return tr;
@@ -47,7 +62,18 @@ createTableBodyRow = rowNum => {
     const cell = document.createElement(`${i === 0 ? "th" : "td"}`);
     if (i === 0) {
       cell.contentEditable = false;
-      cell.innerHTML = rowNum;
+      const span = document.createElement("span");
+      const dropDownDiv = document.createElement("div");
+      span.innerHTML = rowNum;
+      dropDownDiv.setAttribute("class", "dropdown");
+      dropDownDiv.innerHTML = `<button class="dropbtn" id="row-dropbtn-${rowNum}">V</button>
+        <div id="row-dropdown-${rowNum}" class="dropdown-content">
+          <p class="row-insert-top">Insert 1 row above</p>
+          <p class="row-insert-bottom">Insert 1 row below</p>
+          <p class="row-delete">Delete row</p>
+        </div>`;
+      cell.appendChild(span);
+      cell.appendChild(dropDownDiv);
       cell.setAttribute("class", "row-header");
     } else {
       cell.contentEditable = true;
@@ -66,7 +92,6 @@ createTableBody = tableBody => {
 };
 
 populateTable = () => {
-  //   console.log("called once populateTable");
   const data = this.getData();
   if (data === undefined || data === null) return;
 
@@ -93,17 +118,73 @@ addRow = (currentRow, direction) => {
   this.createSpreadsheet();
 };
 
+deleteRow = currentRow => {
+  let data = this.getData();
+  data.splice(currentRow, 1);
+  defaultRowCount++;
+  saveData(data);
+  this.createSpreadsheet();
+};
+
+addColumn = (currentCol, direction) => {
+  let data = this.getData();
+  for (let i = 0; i <= defaultRowCount; i++) {
+    if (direction === "left") {
+      data[i].splice(currentCol, 0, "");
+    } else if (direction === "right") {
+      data[i].splice(currentCol + 1, 0, "");
+    }
+  }
+  defaultColCount++;
+  saveData(data);
+  this.createSpreadsheet();
+};
+
+deleteColumn = currentCol => {
+  let data = this.getData();
+  for (let i = 0; i <= defaultRowCount; i++) {
+    data[i].splice(currentCol, 1);
+  }
+  defaultColCount++;
+  saveData(data);
+  this.createSpreadsheet();
+};
+
+const sortingHistory = new Map();
+sortColumn = currentCol => {
+  let data = this.getData();
+  if (sortingHistory.has(currentCol)) {
+    const sortOrder = sortingHistory.get(currentCol);
+    switch (sortOrder) {
+      case "asc":
+        data.sort((a, b) => a[currentCol] - b[currentCol]);
+        sortingHistory.set(currentCol, "desc");
+        break;
+      case "desc":
+        data.sort((a, b) => b[currentCol] - a[currentCol]);
+        sortingHistory.set(currentCol, "asc");
+        break;
+    }
+  } else {
+    sortingHistory.set(currentCol, "asc");
+    data.sort((a, b) => a[currentCol] - b[currentCol]);
+  }
+  saveData(data);
+  this.createSpreadsheet();
+};
+
 createSpreadsheet = () => {
-  //   const table = document.getElementById("table-main");
   const spreadsheetData = this.getData();
   defaultRowCount = spreadsheetData.length - 1 || defaultRowCount;
   defaultColCount = spreadsheetData[0].length - 1 || defaultColCount;
 
-  const tableHeaders = document.getElementById("table-headers");
+  const tableHeaderElement = document.getElementById("table-headers");
   const tableBodyElement = document.getElementById("table-body");
 
   const tableBody = tableBodyElement.cloneNode(true);
   tableBodyElement.parentNode.replaceChild(tableBody, tableBodyElement);
+  const tableHeaders = tableHeaderElement.cloneNode(true);
+  tableHeaderElement.parentNode.replaceChild(tableHeaders, tableHeaderElement);
 
   tableHeaders.innerHTML = "";
   tableBody.innerHTML = "";
@@ -121,16 +202,71 @@ createSpreadsheet = () => {
       let spreadsheetData = getData();
       spreadsheetData[indices[0]][indices[1]] = item.innerHTML;
       saveData(spreadsheetData);
-      //   console.log("on blur", spreadsheetData[indices[0]][indices[1]]);
     }
   });
+
   tableBody.addEventListener("click", function(e) {
-    if (e.target && e.target.className === "row-header") {
-      // if (e.target && e.target.nodeName === "TH") {
-      console.log("click", e.target.id);
-      addRow(parseInt(e.target.id.split("-")[0]), "bottom");
+    if (e.target) {
+      if (e.target.className === "dropbtn") {
+        const idArr = e.target.id.split("-");
+        document
+          .getElementById(`row-dropdown-${idArr[2]}`)
+          .classList.toggle("show");
+      }
+      if (e.target.className === "row-insert-top") {
+        const indices = e.target.parentNode.id.split("-");
+        addRow(parseInt(indices[2]), "top");
+      }
+      if (e.target.className === "row-insert-bottom") {
+        const indices = e.target.parentNode.id.split("-");
+        addRow(parseInt(indices[2]), "bottom");
+      }
+      if (e.target.className === "row-delete") {
+        const indices = e.target.parentNode.id.split("-");
+        deleteRow(parseInt(indices[2]));
+      }
+    }
+  });
+
+  tableHeaders.addEventListener("click", function(e) {
+    if (e.target) {
+      if (e.target.className === "column-header") {
+        sortColumn(parseInt(e.target.id.split("-")[1]));
+      }
+      if (e.target.className === "dropbtn") {
+        const idArr = e.target.id.split("-");
+        document
+          .getElementById(`col-dropdown-${idArr[2]}`)
+          .classList.toggle("show");
+      }
+      if (e.target.className === "col-insert-left") {
+        const indices = e.target.parentNode.id.split("-");
+        addColumn(parseInt(indices[2]), "left");
+      }
+      if (e.target.className === "col-insert-right") {
+        const indices = e.target.parentNode.id.split("-");
+        addColumn(parseInt(indices[2]), "right");
+      }
+      if (e.target.className === "col-delete") {
+        const indices = e.target.parentNode.id.split("-");
+        deleteColumn(parseInt(indices[2]));
+      }
     }
   });
 };
 
 createSpreadsheet();
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
