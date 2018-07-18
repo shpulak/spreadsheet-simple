129594400c927dcd219d87b57924f50d20e1df6c
Shashwat <shashwat.pulak@gmail.com>
2018-07-19 01:48:39 +0530

    Create add Row function

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const defaultRowCount = 5; // No of rows
-const defaultColCount = 6; // No of cols
+let defaultRowCount = 5; // No of rows
+let defaultColCount = 6; // No of cols
 const SPREADSHEET_DB = "spreadsheet_db";
 
 initializeData = () => {
@@ -30,6 +30,7 @@ saveData = data => {
 
 createHeaderRow = () => {
   const tr = document.createElement("tr");
+  tr.setAttribute("id", 0);
   for (let i = 0; i <= defaultColCount; i++) {
     const th = document.createElement("th");
     th.setAttribute("id", `0-${i}`);
@@ -41,11 +42,13 @@ createHeaderRow = () => {
 
 createTableBodyRow = rowNum => {
   const tr = document.createElement("tr");
+  tr.setAttribute("id", rowNum);
   for (let i = 0; i <= defaultColCount; i++) {
     const cell = document.createElement(`${i === 0 ? "th" : "td"}`);
     if (i === 0) {
       cell.contentEditable = false;
       cell.innerHTML = rowNum;
+      cell.setAttribute("class", "row-header");
     } else {
       cell.contentEditable = true;
     }
@@ -63,7 +66,7 @@ createTableBody = tableBody => {
 };
 
 populateTable = () => {
-  console.log("called once populateTable");
+  //   console.log("called once populateTable");
   const data = this.getData();
   if (data === undefined || data === null) return;
 
@@ -75,14 +78,35 @@ populateTable = () => {
   }
 };
 
-addRow = (currentRow, nextRow) => {
+addRow = (currentRow, direction) => {
   // Change data in storage from current Row;
+  let data = this.getData();
+  const colCount = data[0].length;
+  const newRow = new Array(colCount).fill("");
+  if (direction === "top") {
+    data.splice(currentRow, 0, newRow);
+  } else if (direction === "bottom") {
+    data.splice(currentRow + 1, 0, newRow);
+  }
+  defaultRowCount++;
+  saveData(data);
+  this.createSpreadsheet();
 };
 
-(function createSpreadsheet() {
+createSpreadsheet = () => {
   //   const table = document.getElementById("table-main");
+  const spreadsheetData = this.getData();
+  defaultRowCount = spreadsheetData.length - 1 || defaultRowCount;
+  defaultColCount = spreadsheetData[0].length - 1 || defaultColCount;
+
   const tableHeaders = document.getElementById("table-headers");
-  const tableBody = document.getElementById("table-body");
+  const tableBodyElement = document.getElementById("table-body");
+
+  const tableBody = tableBodyElement.cloneNode(true);
+  tableBodyElement.parentNode.replaceChild(tableBody, tableBodyElement);
+
+  tableHeaders.innerHTML = "";
+  tableBody.innerHTML = "";
 
   tableHeaders.appendChild(createHeaderRow(defaultColCount));
   createTableBody(tableBody, defaultRowCount, defaultColCount);
@@ -100,4 +124,13 @@ addRow = (currentRow, nextRow) => {
       //   console.log("on blur", spreadsheetData[indices[0]][indices[1]]);
     }
   });
-})();
+  tableBody.addEventListener("click", function(e) {
+    if (e.target && e.target.className === "row-header") {
+      // if (e.target && e.target.nodeName === "TH") {
+      console.log("click", e.target.id);
+      addRow(parseInt(e.target.id.split("-")[0]), "bottom");
+    }
+  });
+};
+
+createSpreadsheet();
